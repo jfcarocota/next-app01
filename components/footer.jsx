d54120e181fe0b58e5d6477f91c1b67e52c4a2f4
jfcarocota <jfcarocota@gmail.com>
2021-01-15 00:48:47 -0700

    footer cols

--- components/footer.jsx
+++ components/footer.jsx
@@ -8,19 +8,44 @@ export default class Footer extends Component {
             backgroundColor: '#ff851b'
         }}>
             <Container style={{minHeight: 400}}>
-            <Grid.Row style={{paddingTop: '3em'}}>
-                <Grid.Column width={8}>
-                    <List link inverted style={{color: 'white'}} size='large'>
-                        <List.Header as='h3' content='Contacto'/>
-                        <List.Item as='a'>Enviar un correo</List.Item>
-                        <List.Item as='a'>Sobre nosotros</List.Item>
-                        <List.Item as='a'>Únete al equipo espongoso uwu</List.Item>
-                        <List.Item as='a'>Aviso legal</List.Item>
-                    </List>
-                </Grid.Column>
-                <Grid.Column width={8}>
-                </Grid.Column>
-            </Grid.Row>
+                <Grid style={{paddingTop: '8em'}} centered columns={4}>
+                    <Grid.Column>
+                        <List link inverted style={{color: 'white'}} size='large'>
+                            <List.Header as='h3' content='Contacto'/>
+                            <List.Item as='a'>Enviar un correo</List.Item>
+                            <List.Item as='a'>Sobre nosotros</List.Item>
+                            <List.Item as='a'>Únete al equipo espongoso uwu</List.Item>
+                            <List.Item as='a'>Aviso legal</List.Item>
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <List link inverted style={{color: 'white'}} size='large'>
+                            <List.Header as='h3' content='Contacto'/>
+                            <List.Item as='a'>Enviar un correo</List.Item>
+                            <List.Item as='a'>Sobre nosotros</List.Item>
+                            <List.Item as='a'>Únete al equipo espongoso uwu</List.Item>
+                            <List.Item as='a'>Aviso legal</List.Item>
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <List link inverted style={{color: 'white'}} size='large'>
+                            <List.Header as='h3' content='Contacto'/>
+                            <List.Item as='a'>Enviar un correo</List.Item>
+                            <List.Item as='a'>Sobre nosotros</List.Item>
+                            <List.Item as='a'>Únete al equipo espongoso uwu</List.Item>
+                            <List.Item as='a'>Aviso legal</List.Item>
+                        </List>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <List link inverted style={{color: 'white'}} size='large'>
+                            <List.Header as='h3' content='Contacto'/>
+                            <List.Item as='a'>Enviar un correo</List.Item>
+                            <List.Item as='a'>Sobre nosotros</List.Item>
+                            <List.Item as='a'>Únete al equipo espongoso uwu</List.Item>
+                            <List.Item as='a'>Aviso legal</List.Item>
+                        </List>
+                    </Grid.Column>
+                </Grid>
             </Container>
         </footer>
     );
